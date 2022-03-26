72f7aaa9bfa6f01c851b9bb005a315e5e7ec9201
Ugur-Mert <u.mert.ozderr@gmail.com>
2022-03-26 04:05:04 +0300

    components are added

---
+++ src/MainContent.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function MainContent() {
+  return (
+    <div>
+      <h1>Why I am excited to learn react </h1>
+
+      <ul>
+        <li>Because so handy</li>
+        <li>More work opportunity</li>
+        <li>Better than other libraries.</li>
+      </ul>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,45 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-
+import Header from "./Header";
+import Footer from "./Footer";
+import MainContent from "./MainContent";
 import reportWebVitals from "./reportWebVitals";
-
-function Header() {
-  return (
-    <header>
-      <nav className="nav">
-        <img className="react-img" src="../logo512.png" alt="react-logo"></img>
-        <ul className="nav-items">
-          <li>Pricing</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
-    </header>
-  );
-}
-
-function MainContent() {
-  return (
-    <div>
-      <h1>Why I am excited to learn react </h1>
-
-      <ul>
-        <li>Because so handy and useful</li>
-        <li>More work opportunity</li>
-        <li>Better than vue.</li>
-      </ul>
-    </div>
-  );
-}
-
-function Footer() {
-  return (
-    <footer>
-      <small>© 2022 Ozder development. All rights reserved</small>
-    </footer>
-  );
-}
 
 function WhyExcited() {
   return (
